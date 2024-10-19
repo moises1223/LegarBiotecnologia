fc47be87b2c435e3e472339c7b553ddce6807123
Moisés Souza <daorahum@gmail.com>
2024-10-18 23:36:21 -0300

    alteraçções

--- js/carroussel.js
+++ js/carroussel.js
@@ -1,73 +1,79 @@
-const createCarroussel = () =>{
+    const createCarroussel = () =>{
     let product = document.querySelector('.card-wrapper');
 
     product.innerHTML=`
     <ul class="swiper-wrapper card-list ">
-                        <li class="card-item swiper-slide">
-                            <a href="/Public_html//produtos.html" class="card-link">
-                                <div class="img-product">
-                                    <img src="img/produto.png" class="card-img" alt="Card Image" >
-                                </div>
-                                <h2 class="card-title">EnerBlu</h2>
-                                <div class="block">
-                                    <p class="description"> Promotor de crescimento </p>
-                                    <button class="card-button ">VER </button>
 
-                                </div>
-                            </a>
-                        </li>
                         <li class="card-item swiper-slide">
                             <a href="/Public_html/produtos.html" class="card-link">
                                 <div class="img-product">
                                     <img src="img/produto.png" class="card-img" alt="Card Image" >
                                 </div>
-                                <h2 class="card-title">EnerBlu</h2>
+                                <h2 class="card-title">Option One Pró </h2>
                                 <div class="block">
-                                    <p class="description"> Promotor de crescimento </p>
+                                    <p class="description">Aminoácido concentrado e de rápida absorção </p>
+                                    <button class="card-button "> VER </button>
+
+                                </div>
+                            </a>
+                        </li>
+
+                        <li class="card-item swiper-slide">
+                            <a href="/Public_html/produtos.html" class="card-link">
+                                <div class="img-product">
+                                    <img src="img/produto.png" class="card-img" alt="Card Image" >
+                                </div>
+                                <h2 class="card-title">Option One Pró</h2>
+                                <div class="block">
+                                    <p class="description">Aminoácido concentrado e de rápida absorção </p>
                                     <button class="card-button ">VER </button>
 
                                 </div>
                             </a>
                         </li>
+
                         <li class="card-item swiper-slide">
                             <a href="/Public_html//produtos.html" class="card-link">
                                 <div class="img-product">
                                     <img src="img/produto.png" class="card-img" alt="Card Image" >
                                 </div>
-                                <h2 class="card-title">EnerBlu</h2>
+                                <h2 class="card-title">Option One Pró</h2>
                                 <div class="block">
-                                    <p class="description"> Promotor de crescimento </p>
+                                    <p class="description">Aminoácido concentrado e de rápida absorção </p>
                                     <button class="card-button ">VER </button>
 
                                 </div>
                             </a>
                         </li>
+
                         <li class="card-item swiper-slide">
                             <a href="/Public_html//produtos.html" class="card-link">
                                 <div class="img-product">
                                     <img src="img/produto.png" class="card-img" alt="Card Image" >
                                 </div>
-                                <h2 class="card-title">EnerBlu</h2>
+                                <h2 class="card-title">Option One Pró</h2>
                                 <div class="block">
-                                    <p class="description"> Promotor de crescimento </p>
+                                    <p class="description">Aminoácido concentrado e de rápida absorção </p>
                                     <button class="card-button ">VER </button>
 
                                 </div>
                             </a>
                         </li>
+
                         <li class="card-item swiper-slide">
                             <a href="/Public_html//produtos.html" class="card-link">
                                 <div class="img-product">
                                     <img src="img/produto.png" class="card-img" alt="Card Image" >
                                 </div>
-                                <h2 class="card-title">EnerBlu</h2>
+                                <h2 class="card-title">Option One Pró</h2>
                                 <div class="block">
-                                    <p class="description"> Promotor de crescimento </p>
+                                    <p class="description">Aminoácido concentrado e de rápida absorção </p>
                                     <button class="card-button ">VER</button>
 
                                 </div>
                             </a>
                         </li>
+                        
                     </ul>
 
                     <div class="swiper-pagination"></div>
